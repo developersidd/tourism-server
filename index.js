b5eff5cedd16af18c3557bcd9a2fbd6a962e3ce0
AB Siddik <siddik.prgmr@gmail.com>
2023-01-23 07:05:28 +0600

    not found handler added

--- index.js
+++ index.js
@@ -40,199 +40,204 @@ async function verifyToken(req, res, next) {
     next();
 }
 
-async function run() {
+client.connect();
+const database = client.db("AB_tourism");
+const Tourism_slider_data = database.collection("Torism_slider");
+const tour_collection_data = database.collection("tour_collection");
+const clicked_collection = database.collection("clicked_collection");
+const user_collection = database.collection("user_collection");
 
-    try {
-        await client.connect();
-        const database = client.db("AB_tourism");
-        const Tourism_slider_data = database.collection("Torism_slider");
-        const tour_collection_data = database.collection("tour_collection");
-        const clicked_collection = database.collection("clicked_collection");
-        const user_collection = database.collection("user_collection");
+// get all booked tour
+app.get("/ordered_tour/:email", verifyToken, async (req, res) => {
+    const email = req.params.email;
+    //check user is authorized or not
+    if (req?.decodedEmail === email) {
+        // check if the requested user is admin or not
+        const adminUser = await user_collection.findOne({ email: email });
+        if (adminUser.role === 'admin') {
+            const data = await clicked_collection.find({}).toArray();
+            res.status(200).json(data);
+        } else {
+            callback(401, { message: "Authorization failure!" })
+        }
+    } else {
+        res.status(403).json({ message: "Authentication failed!" });
+    }
+});
 
-        // get all booked tour
-        app.get("/ordered_tour/:email", verifyToken, async (req, res) => {
-            const email = req.params.email;
-            //check user is authorized or not
-            if (req?.decodedEmail === email) {
-                // check if the requested user is admin or not
-                const adminUser = await user_collection.findOne({ email: email });
-                if (adminUser.role === 'admin') {
-                    const data = await clicked_collection.find({}).toArray();
-                    res.status(200).json(data);
-                } else {
-                    callback(401, { message: "Authorization failure!" })
-                }
-            } else {
-                res.status(403).json({ message: "Authentication failed!" });
-            }
-        });
-
-        // get slider data from db
-        app.get("/sliders_data", async (req, res) => {
-            const result = await Tourism_slider_data.find({}).toArray();
-            res.json(result);
-        });
+// get slider data from db
+app.get("/sliders_data", async (req, res) => {
+    const result = await Tourism_slider_data.find({}).toArray();
+    res.json(result);
+});
 
 
-        // get tour packages from db
-        app.get("/tours", async (req, res) => {
-            const result = await tour_collection_data.find({}).toArray();
-            res.json(result);
-        });
+// get tour packages from db
+app.get("/tours", async (req, res) => {
+    const result = await tour_collection_data.find({}).toArray();
+    res.json(result);
+});
 
-        //// get my_orders from db
-        app.get("/users/:email", verifyToken, async (req, res) => {
-            const { email } = req.params;
-            if (req?.decodedEmail === email) {
-                const result = await clicked_collection.find({ email: email }).toArray();
-                res.status(200).json(result);
-            } else {
-                res.status(403).json({ message: "Authentication failed!" });
-            }
-        });
+//// get my_orders from db
+app.get("/users/:email", verifyToken, async (req, res) => {
+    const { email } = req.params;
+    if (req?.decodedEmail === email) {
+        const result = await clicked_collection.find({ email: email }).toArray();
+        res.status(200).json(result);
+    } else {
+        res.status(403).json({ message: "Authentication failed!" });
+    }
+});
 
-        // get a tour using id
-        app.get("/single_pd/:id/:email", verifyToken, async (req, res) => {
-            const { id } = req.params;
-            const email = req.params.email;
-            if (req?.decodedEmail === email) {
-                const result = await tour_collection_data.findOne({ _id: ObjectId(id) });
-                res.status(200).json(result);
-            } else {
-                res.status(403).json({ message: "Authentication failed!" });
-            }
-        });
+// get a tour using id
+app.get("/single_pd/:id/:email", verifyToken, async (req, res) => {
+    const { id } = req.params;
+    const email = req.params.email;
+    if (req?.decodedEmail === email) {
+        const result = await tour_collection_data.findOne({ _id: ObjectId(id) });
+        res.status(200).json(result);
+    } else {
+        res.status(403).json({ message: "Authentication failed!" });
+    }
+});
 
-        // check admin role
-        app.get("/check_admin/:email", verifyToken, async (req, res) => {
-            const { email } = req.params;
-            if (req.decodedEmail === email) {
-                let isAdmin = false;
-                const result = await user_collection.findOne({ email: email });
-                if (result?.role === "admin") {
-                    isAdmin = true;
-                    res.status(200).json({ isAdmin });
-                } else {
+// check admin role
+app.get("/check_admin/:email", verifyToken, async (req, res) => {
+    const { email } = req.params;
+    if (req.decodedEmail === email) {
+        let isAdmin = false;
+        const result = await user_collection.findOne({ email: email });
+        if (result?.role === "admin") {
+            isAdmin = true;
+            res.status(200).json({ isAdmin });
+        } else {
 
-                }
-            } else {
-                res.status(403).json({ message: "Authentication failed!" });
-            }
-        });
+        }
+    } else {
+        res.status(403).json({ message: "Authentication failed!" });
+    }
+});
 
-        // delete a tour
-        app.delete("/:tourId/:email", verifyToken, async (req, res) => {
-            const id = req.params.tourId;
-            const email = req.params.email;
-            if (req?.decodedEmail === email) {
-                const result = await clicked_collection.deleteOne({ _id: ObjectId(id) });
-                res.status(200).json(result);
-            }
-            else {
-                callback(403, { message: "Authentication failure!" })
-            }
-        });
+// delete a tour
+app.delete("/:tourId/:email", verifyToken, async (req, res) => {
+    const id = req.params.tourId;
+    const email = req.params.email;
+    if (req?.decodedEmail === email) {
+        const result = await clicked_collection.deleteOne({ _id: ObjectId(id) });
+        res.status(200).json(result);
+    }
+    else {
+        callback(403, { message: "Authentication failure!" })
+    }
+});
 
-        // delete a booked tour for admin
-        app.delete("delete_tour_admin/:tourId/:email", verifyToken, async (req, res) => {
-            const id = req.params.tourId;
-            const email = req.params.email;
+// delete a booked tour for admin
+app.delete("delete_tour_admin/:tourId/:email", verifyToken, async (req, res) => {
+    const id = req.params.tourId;
+    const email = req.params.email;
 
-            //check user is authorized or not
-            if (req.decodedEmail === email) {
-                // check if the requested user is admin or not
-                const adminUser = await user_collection.findOne({ email: req.params.email });
-                if (adminUser.role === 'admin') {
-                    const result = await clicked_collection.deleteOne({ _id: ObjectId(id) });
-                    res.status(200).json(result);
-                } else {
-                    callback(401, { message: "Authorization failure!" })
-                }
-            } else {
-                callback(403, { message: "Authentication failure!" })
-            }
-        });
-
-        // add registered user 
-        app.post("/add_user", async (req, res) => {
-            const user = req.body;
-            const result = await user_collection.insertOne(user);
+    //check user is authorized or not
+    if (req.decodedEmail === email) {
+        // check if the requested user is admin or not
+        const adminUser = await user_collection.findOne({ email: req.params.email });
+        if (adminUser.role === 'admin') {
+            const result = await clicked_collection.deleteOne({ _id: ObjectId(id) });
             res.status(200).json(result);
-        });
+        } else {
+            callback(401, { message: "Authorization failure!" })
+        }
+    } else {
+        callback(403, { message: "Authentication failure!" })
+    }
+});
 
-        // add google user
-        app.put("/add_user", async (req, res) => {
-            const user = req.body;
-            const filter = { email: user.email };
-            const updateDoc = { $set: user };
-            const option = { upsert: true };
+// add registered user 
+app.post("/add_user", async (req, res) => {
+    const user = req.body;
+    const result = await user_collection.insertOne(user);
+    res.status(200).json(result);
+});
+
+// add google user
+app.put("/add_user", async (req, res) => {
+    const user = req.body;
+    const filter = { email: user.email };
+    const updateDoc = { $set: user };
+    const option = { upsert: true };
+    const result = await user_collection.updateOne(filter, updateDoc, option);
+    console.log("result", result)
+
+    res.status(200).json(result);
+});
+
+// Make admin for admin
+app.put("/make_admin/:email", verifyToken, async (req, res) => {
+    const email = req.params.email;
+    const filter = { email: req.body?.email };
+    const updateDoc = { $set: { role: "admin" } };
+    const option = { upsert: true };
+    //check user is authorized or not
+    if (req.decodedEmail === email) {
+        // check if the requested user is admin or not
+        const adminUser = await user_collection.findOne({ email: email });
+        if (adminUser?.role === 'admin') {
             const result = await user_collection.updateOne(filter, updateDoc, option);
-            console.log("result", result)
-
             res.status(200).json(result);
-        });
-
-        // Make admin for admin
-        app.put("/make_admin/:email", verifyToken, async (req, res) => {
-            const email = req.params.email;
-            const filter = { email: req.body?.email };
-            const updateDoc = { $set: { role: "admin" } };
-            const option = { upsert: true };
-            //check user is authorized or not
-            if (req.decodedEmail === email) {
-                // check if the requested user is admin or not
-                const adminUser = await user_collection.findOne({ email: email });
-                if (adminUser?.role === 'admin') {
-                    const result = await user_collection.updateOne(filter, updateDoc, option);
-                    res.status(200).json(result);
-                } else {
-                    callback(401, { message: "Authorization failure!" })
-                }
-            } else {
-                callback(403, { message: "Authentication failure!" })
-            }
-
-        });
-
-        // add a Order Collection 
-        app.post("/add_a_collection/:email", verifyToken, async (req, res) => {
-            const tour = req.body;
-            const email = req.params.email;
-            if (req.decodedEmail === email) {
-                const result = await clicked_collection.insertOne(tour);
-                res.json(result);
-            } else {
-                callback(403, { message: "Authentication failure!" })
-            }
-        });
-
-        // add a tour for admin 
-        app.post("/add_tour/:email", verifyToken, async (req, res) => {
-            const newTour = req.body;
-            const email = req.params.email;
-            //check user is authorized or not
-            if (req?.decodedEmail === email) {
-                // check if the requested user is admin or not
-                const adminUser = await user_collection.findOne({ email: email });
-                if (adminUser?.role === 'admin') {
-                    const result = await tour_collection_data.insertOne(newTour);
-                    res.status(200).json(result);
-                } else {
-                    callback(401, { message: "Authorization failure!" })
-                }
-            } else {
-                callback(403, { message: "Authentication failure!" })
-            }
-        });
+        } else {
+            callback(401, { message: "Authorization failure!" })
+        }
+    } else {
+        callback(403, { message: "Authentication failure!" })
     }
 
-    //end
-    finally {
-    }
+});
 
-}
-run().catch((err) => console.log(err));
+// add a Order Collection 
+app.post("/add_a_collection/:email", verifyToken, async (req, res) => {
+    const tour = req.body;
+    const email = req.params.email;
+    if (req.decodedEmail === email) {
+        const result = await clicked_collection.insertOne(tour);
+        res.json(result);
+    } else {
+        callback(403, { message: "Authentication failure!" })
+    }
+});
+
+// add a tour for admin 
+app.post("/add_tour/:email", verifyToken, async (req, res) => {
+    const newTour = req.body;
+    const email = req.params.email;
+    //check user is authorized or not
+    if (req?.decodedEmail === email) {
+        // check if the requested user is admin or not
+        const adminUser = await user_collection.findOne({ email: email });
+        if (adminUser?.role === 'admin') {
+            const result = await tour_collection_data.insertOne(newTour);
+            res.status(200).json(result);
+        } else {
+            callback(401, { message: "Authorization failure!" })
+        }
+    } else {
+        callback(403, { message: "Authentication failure!" })
+    }
+});
+
+// 404 Error Handler
+app.use((req, res, next) => {
+    const error = new Error("The requested page does not exist!");
+    error.status = 404;
+    res.status(error.status).json(error.message);
+});
+
+//Default Error Handler
+app.use((err, req, res, next) => {
+    if (res.headersSent) {
+        next(err);
+    };
+    res.status(err.status || 500).json(err.message || "There was an Error");
+});
+
 
 app.get("/", async (req, res) => {
     res.json("Running");
